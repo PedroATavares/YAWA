80fe17bf974ddbb2557b2a32ac660e99179a0cba
Dude29 <joaopc29@hotmail.com>
2016-10-17 09:47:17 +0100

    Remove unused files
    Unused files leftover from initial project showed in class have been removed

--- YAWA/app/src/main/kotlin/isel/pdm/demos/mymoviedb/models/MovieDto.kt
+++
@@ -1,9 +0,0 @@
-package isel.pdm.demos.mymoviedb.models
-
-
-/**
- * Class whose instances represent movie information obtained from the remote API.
- */
-data class MovieDto(val adult: Boolean, val title: String, val id: Long) {
-    private constructor() : this(false, "", 0)
-}
