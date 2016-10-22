4963ad1c14592171587e7beadfbe41c4a4db233f
Dude29 <joaopc29@hotmail.com>
2016-10-23 00:35:05 +0100

    Minor refactor on MainActivity

--- YAWA/app/src/main/kotlin/isel/yawa/MainActivity.kt
+++ YAWA/app/src/main/kotlin/isel/yawa/MainActivity.kt
@@ -4,13 +4,10 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-    val apikey = "&appid=64ebb6f1a2a7abf9c9a91fedf34426dd"
-    val apihttp = "http://api.openweathermap.org/data/2.5/weather?q="
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,18 +15,34 @@ class MainActivity : AppCompatActivity() {
 
 
         creditsButton.setOnClickListener{
-            this.startActivity(Intent(this, CreditsActivity::class.java));
+            startActivity(Intent(this, CreditsActivity::class.java));
         }
 
-        editText.setOnEditorActionListener(TextView.OnEditorActionListener { v, actionId, event ->
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                val city =v.text.toString()
-                val myIntent = Intent(this@MainActivity, WeatherActivity::class.java)
-                myIntent.putExtra("url", apihttp+city+apikey)
-                this@MainActivity.startActivity(myIntent)
-                return@OnEditorActionListener true
-            }
-            false
+        editText.setOnEditorActionListener({ tv, actionId, kev ->
+            var enterPressed = actionId == EditorInfo.IME_ACTION_DONE
+
+            if (enterPressed)
+                getCurrentWeatherForCity(tv.text.toString())
+
+            return@setOnEditorActionListener enterPressed // kotlin is weird, man
         })
     }
+
+    private fun getCurrentWeatherForCity(city : String) {
+        val myIntent = Intent(this, WeatherActivity::class.java)
+
+        var qString : String = buildQueryString(city)
+        myIntent.putExtra("url", qString)
+
+        startActivity(myIntent)
+    }
+
+    private fun  buildQueryString(city: String): String {
+        var api_base = resources.getString(R.string.api_base_uri)
+        var api_weather = resources.getString(R.string.api_weather_endpoint)
+        var api_key = resources.getString(R.string.api_key)
+
+        return "${api_base}${api_weather}?${api_key}&q=${city}"
+    }
+
 }
