d1bad563ac3a5413e03cba6f304a8140889d9c8b
Dude29 <joaopc29@hotmail.com>
2016-12-28 17:56:28 +0000

    Changed Notification to service now named NotificationEmitter

--- YAWA/app/src/main/kotlin/isel/yawa/Application.kt
+++ YAWA/app/src/main/kotlin/isel/yawa/Application.kt
@@ -6,7 +6,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import isel.yawa.connect.RequestManager
 import isel.yawa.model.services.CurrentWeatherService
-import isel.yawa.broadcastReceivers.Notification
+import isel.yawa.model.services.NotificationEmitter
 import java.util.*
 
 class Application : android.app.Application() {
@@ -21,8 +21,8 @@ class Application : android.app.Application() {
     }
 
     fun scheduleUpdate(cities: ArrayList<String>?, repeat_Time: Long) {
-        val action = Intent(this, CurrentWeatherService::class.java)
-                .putExtra("cities", cities)
+        val action = Intent(this, CurrentWeatherService::class.java).putExtra("cities", cities)
+
         (getSystemService(AppCompatActivity.ALARM_SERVICE) as AlarmManager).setInexactRepeating(
                 AlarmManager.ELAPSED_REALTIME_WAKEUP,
                 0,
@@ -31,24 +31,29 @@ class Application : android.app.Application() {
         )
     }
 
+    /**
+     * Schedules a notification to show the weather for the city passed as parameter
+     */
     fun scheduleNotification(city: String, hour: Int, minute: Int) {
-        val cal = Calendar.getInstance()
-        cal.set(Calendar.HOUR_OF_DAY, hour)
-        cal.set(Calendar.MINUTE, minute)
-        val TriggerMillis = cal.timeInMillis
-        val action = Intent(this, Notification::class.java)
-                .putExtra("city", city)
-        (getSystemService(AppCompatActivity.ALARM_SERVICE) as AlarmManager).set(
-                AlarmManager.RTC_WAKEUP,
-                TriggerMillis,
-                PendingIntent.getBroadcast(this, 2, action, PendingIntent.FLAG_UPDATE_CURRENT)
-        )
+        val cal = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY, hour)
+            set(Calendar.MINUTE, minute)
+        }
 
+        val action = Intent(this, NotificationEmitter::class.java).putExtra(CITY_KEY, city)
+
+        val alarmManager = getSystemService(AppCompatActivity.ALARM_SERVICE) as AlarmManager
+        alarmManager.set(
+                AlarmManager.RTC_WAKEUP,
+                cal.timeInMillis,
+                PendingIntent.getService(this, 2, action, PendingIntent.FLAG_UPDATE_CURRENT)
+        )
     }
 
     fun cancelNotificationAlarm(){
-        val intent = Intent(this, Notification::class.java)
-        val pendingIntent = PendingIntent.getBroadcast(this, 2, intent, 0)
+        val intent = Intent(this, NotificationEmitter::class.java)
+        val pendingIntent = PendingIntent.getService(this, 2, intent, 0)
+
         val alarmManager = getSystemService(AppCompatActivity.ALARM_SERVICE) as AlarmManager
         alarmManager.cancel(pendingIntent)
     }
